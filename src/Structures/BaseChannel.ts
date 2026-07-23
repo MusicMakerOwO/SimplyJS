2412aaffa437c8be58c70fbddd58ee937e8a64f2
Music Maker <joshuanejdl@gmail.com>
2026-07-23 11:08:25 -0500

    feat(channels): Added functions to easily tell channel types apart - text, voice, thread, category, etc.

--- src/Structures/BaseChannel.ts
+++ src/Structures/BaseChannel.ts
@@ -3,6 +3,12 @@ import { DiscordChannel, DiscordChannelTypes } from "../Types/DiscordAPITypes.js
 import { ObjectValues } from "../Types/HelperTypes.js";
 import { APIGuildStructure } from "../Contracts/DiscordStructure.js";
 import { Guild } from "./Guild.js";
+import { GuildTextChannel } from "./GuildTextChannel.js";
+import { GuildThreadChannel } from "./GuildThreadChannel.js";
+import { GuildAnnouncementChannel } from "./GuildAnnouncementChannel.js";
+import { GuildStageChannel } from "./GuildStageChannel.js";
+import { GuildVoiceChannel } from "./GuildVoiceChannel.js";
+import { GuildCategoryChannel } from "./GuildCategoryChannel.js";
 
 export class BaseChannel extends APIGuildStructure<DiscordChannel> {
 	id!: string
@@ -22,6 +28,38 @@ export class BaseChannel extends APIGuildStructure<DiscordChannel> {
 		if (data.name !== undefined) this.name = data.name;
 		if (data.flags !== undefined) this.flags = data.flags;
 		if (data.guild_id !== undefined) this.guild_id = data.guild_id;
+	}
+
+	/**
+	 * Tests if the current channel is a text or announcements channel
+	 */
+	isTextChannel(): this is GuildTextChannel | GuildAnnouncementChannel {
+		return this.type === DiscordChannelTypes.GUILD_TEXT ||
+			   this.type === DiscordChannelTypes.GUILD_ANNOUNCEMENT;
+	}
+
+	/**
+	 * Tests if the current channel is a voice or stage channel
+	 */
+	isVoiceChannel(): this is GuildVoiceChannel | GuildStageChannel {
+		return this.type === DiscordChannelTypes.GUILD_VOICE ||
+			   this.type === DiscordChannelTypes.GUILD_STAGE_VOICE;
+	}
+
+	/**
+	 * Tests if the current channel is a category
+	 */
+	isCategoryChannel(): this is GuildCategoryChannel {
+		return this.type === DiscordChannelTypes.GUILD_CATEGORY;
+	}
+
+	/**
+	 * Tests if the current channel is a thread (could be public, private, or announcements - check `Channel.type` for more precission)
+	 */
+	isThreadChannel(): this is GuildThreadChannel {
+		return this.type === DiscordChannelTypes.PUBLIC_THREAD ||
+			   this.type === DiscordChannelTypes.PRIVATE_THREAD ||
+			   this.type === DiscordChannelTypes.ANNOUNCEMENT_THREAD;
 	}
 
 	async delete(): Promise<void> {
