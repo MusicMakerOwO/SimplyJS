1a4993e110f6c0cf2edb1c62ec95b9342ae0384d
Music Maker <joshuanejdl@gmail.com>
2026-07-28 18:55:43 -0500

    feat(bans): Updated fetch() to also grab in bulk

--- src/Managers/GuildBans.ts
+++ src/Managers/GuildBans.ts
@@ -20,11 +20,47 @@ export class GuildBanManager {
 		await this.client.rest.delete(`/guilds/${this.guild.id}/bans/${this.guild.id}`, reason ? { 'X-Audit-Log-Reason': reason } : {} );
 	}
 
-	async fetch(id: string): Promise<{ user: User, reason: string | null }> {
-		const response = await this.client.rest.get<{ user: DiscordUser, reason: string | null }>(`/guilds/${this.guild.id}/bans/${id}`);
-		return {
-			user: this.client.users.upsert(response.user),
-			reason: response.reason
+	async fetch(id: string): Promise<{ user: User, reason: string | null }>;
+	async fetch(options?: {
+		/** Number of users to return (up to 1000, default 1000) */
+		limit?: number;
+		/** Consider only users before given user ID - Useful in batching  */
+		before?: string;
+		/** Consider only users after given user ID - Useful in batching  */
+		after?: string
+	}): Promise<{ user: User, reason: string | null }[]>;
+	async fetch(options?: string | { limit?: number; before?: string; after?: string }): Promise<{ user: User, reason: string | null } | { user: User, reason: string | null }[]> {
+		if (typeof options === 'string') {
+			const response = await this.client.rest.get<{
+				user: DiscordUser,
+				reason: string | null
+			}>(`/guilds/${this.guild.id}/bans/${options}`);
+			return {
+				user: this.client.users.upsert(response.user),
+				reason: response.reason
+			}
+		} else {
+			const params = new URLSearchParams();
+			if (options?.limit) params.append('limit', options.limit.toString());
+			if (options?.before) params.append('before', options.before);
+			if (options?.after) params.append('after', options.after);
+			const query = params.toString()
+				? `?${params.toString()}`
+				: '';
+
+			const response = await this.client.rest.get<{
+				user: DiscordUser,
+				reason: string | null
+			}[]>(`/guilds/${this.guild.id}/bans${query}`);
+			const list = new Array(response.length);
+			for (let i = 0; i < response.length; i++) {
+				const ban = response[i];
+				list[i] = {
+					user: this.client.users.upsert(ban.user),
+					reason: ban.reason
+				}
+			}
+			return list;
 		}
 	}
 }
