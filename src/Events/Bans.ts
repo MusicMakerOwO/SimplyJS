5976adc78e4493907a42cbad397b233e6529e07a
Music Maker <joshuanejdl@gmail.com>
2026-08-15 15:55:47 -0500

    fix(bans): Guild ban event handlers no longer assume the guild is cached

--- src/Events/Bans.ts
+++ src/Events/Bans.ts
@@ -3,7 +3,8 @@ import { ClientEvents, defineEvent, DiscordUser, GatewayEvents } from "../Types/
 export const GuildBanAdd = defineEvent({
 	name: GatewayEvents.GuildBanAdd,
 	handler: (client, data: { guild_id: string, user: DiscordUser }) => {
-		const guild = client.guilds.get(data.guild_id)!;
+		const guild = client.guilds.get(data.guild_id);
+		if (!guild) return;
 		const user = client.users.upsert(data.user);
 		client.emit(ClientEvents.GuildBanAdd, guild, user);
 	}
@@ -12,7 +13,8 @@ export const GuildBanAdd = defineEvent({
 export const GuildBanRemove = defineEvent({
 	name: GatewayEvents.GuildBanRemove,
 	handler: (client, data: { guild_id: string, user: DiscordUser }) => {
-		const guild = client.guilds.get(data.guild_id)!;
+		const guild = client.guilds.get(data.guild_id);
+		if (!guild) return;
 		const user = client.users.upsert(data.user);
 		client.emit(ClientEvents.GuildBanRemove, guild, user);
 	}
